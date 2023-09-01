d94b1f780e4320ab5bc63d23afb66060e92d2e8d
JINGzmr <2743377630@qq.com>
2023-09-01 16:08:59 +0800

    feat:Project Completion

--- others/IO.cc
+++ others/IO.cc
@@ -30,26 +30,6 @@ int SendMsg::writen(int fd, char *msg, int size)
 // 客户端发送序列化好的数据
 void SendMsg::SendMsg_client(int client_socket, const string &str)
 {
-  // int DataSize = htonl(str.length()); // 发给服务端的包的大小
-
-  // if (send(client_socket, &DataSize, sizeof(int), 0) == -1)
-  // {
-  //     std::cout << "client send data size failed" << std::endl;
-  // }
-  // else
-  // {
-  //     std::cout << "client data sent to the server" << std::endl;
-  // }
-
-  // if (send(client_socket, str.c_str(), str.size(), 0) == -1)
-  // {
-  //     std::cout << "client send failed" << std::endl;
-  // }
-  // else
-  // {
-  //     std::cout << "client data sent to the server" << std::endl;
-  // }
-
   if (client_socket < 0 || str.c_str() == NULL || str.size() <= 0)
   {
     return;
@@ -66,31 +46,6 @@ void SendMsg::SendMsg_client(int client_socket, const string &str)
     close(client_socket);
   }
 }
-
-// // 服务端发送序列化好的数据
-// void SendMsg::SendMsg_server(int client_socket, const std::string &str)
-// {
-// int DataSize = htonl(str.length()); // 发给服务端的包的大小
-
-// if (send(client_socket, &DataSize, sizeof(int), 0) == -1)
-// {
-//     std::cout << "server send data size failed" << std::endl;
-// }
-// else
-// {
-//     std::cout << "server data sent to the server" << std::endl;
-// }
-
-// if (send(client_socket, str.c_str(), str.size(), 0) == -1)
-// {
-//     std::cout << "server send failed" << std::endl;
-// }
-// else
-// {
-//     std::cout << "server data sent to the server" << std::endl;
-// }
-
-// }
 
 // 服务端发送数据处理的结果（成功/失败）
 void SendMsg::SendMsg_int(int client_socket, int state)
@@ -133,38 +88,6 @@ int RecvMsg::readn(int fd, char *buf, int size)
 // 客户端接收序列化的数据
 int RecvMsg::RecvMsg_client(int client_socket, string &str)
 {
-  // int DataSize = 0; // 从服务端发送过来的包的大小
-  // char buffer[BUFFER_SIZE];
-  // memset(buffer, 0, BUFFER_SIZE);
-
-  // ssize_t recv_bytes = recv(client_socket, &DataSize, sizeof(int), 0);
-  // if (recv_bytes == -1)
-  // {
-  //     std::cout << "client recv data size failed" << std::endl;
-  // }
-  // else if (recv_bytes == 0) // 客户端断开连接
-  // {
-  //     std::cout << "Connection closed by peer." << std::endl;
-  //     close(client_socket); // 先关闭该客户端
-  //     // epoll_ctl(epld, EPOLL_CTL_DEL, client_socket, 0); // 从epoll里面删除，不需要再检测了
-  //     // clients.earse(client_socket); // 同时把他从map里面删除
-  // }
-
-  // recv_bytes = recv(client_socket, buffer, DataSize, 0);
-  // if (recv_bytes == -1)
-  // {
-  //     std::cout << "client recv failed" << std::endl;
-  // }
-  // else if (recv_bytes == 0) // 客户端断开连接
-  // {
-  //     std::cout << "Connection closed by peer." << std::endl;
-  //     close(client_socket); // 先关闭该客户端
-  //     // epoll_ctl(epld, EPOLL_CTL_DEL, client_socket, 0); // 从epoll里面删除，不需要再检测了
-  //     // clients.earse(client_socket); // 同时把他从map里面删除
-  // }
-
-  // str = string(buffer, recv_bytes); // 将字符数组buffer中的recv_bytes个字符复制到字符串str中
-
   int len = 0;
   readn(client_socket, (char *)&len, 4);
   len = ntohl(len);
@@ -184,42 +107,6 @@ int RecvMsg::RecvMsg_client(int client_socket, string &str)
 
   return Len;
 }
-
-// // 服务端接收序列化的数据
-// void RecvMsg::RecvMsg_server(int client_socket, std::string &str)
-// {
-//     int DataSize = 0; // 从服务端发送过来的包的大小
-//     char buffer[BUFFER_SIZE];
-//     memset(buffer, 0, BUFFER_SIZE);
-
-//     ssize_t recv_bytes = recv(client_socket, &DataSize, sizeof(int), 0);
-//     if (recv_bytes == -1)
-//     {
-//         std::cout << "server recv data size failed" << std::endl;
-//     }
-//     else if (recv_bytes == 0) // 客户端断开连接
-//     {
-//         std::cout << "Connection closed by peer." << std::endl;
-//         close(client_socket); // 先关闭该客户端
-//         // epoll_ctl(epld, EPOLL_CTL_DEL, client_socket, 0); // 从epoll里面删除，不需要再检测了
-//         // clients.earse(client_socket); // 同时把他从map里面删除
-//     }
-
-//     recv_bytes = recv(client_socket, buffer, DataSize, 0);
-//     if (recv_bytes == -1)
-//     {
-//         std::cout << "server recv failed" << std::endl;
-//     }
-//     else if (recv_bytes == 0) // 客户端断开连接
-//     {
-//         std::cout << "Connection closed by peer." << std::endl;
-//         close(client_socket); // 先关闭该客户端
-//         // epoll_ctl(epld, EPOLL_CTL_DEL, client_socket, 0); // 从epoll里面删除，不需要再检测了
-//         // clients.earse(client_socket); // 同时把他从map里面删除
-//     }
-
-//     str = string(buffer, recv_bytes); // 将字符数组buffer中的recv_bytes个字符复制到字符串str中
-// }
 
 // 客户端接收数据处理的结果（成功/失败）
 int RecvMsg::RecvMsg_int(int client_socket)
